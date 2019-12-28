da226f9bf5a8d0da776615a2d99f7c95d9bca659
unknown <yinyiwu>
2019-12-28 17:55:34 +0800

    2019-12-28T17:55:34+08:00

--- service/datapull.js
+++ service/datapull.js
@@ -2,6 +2,9 @@ const spawn = require('cross-spawn');
 module.exports = {   
 	gitPush: async function(req, res, next) {
 		spawn.sync('git', ['status'], { stdio: 'inherit' });
+		spawn.sync('git', ['checkout', 'db/store/pcust.db'], { stdio: 'inherit' });
+		spawn.sync('git', ['checkout', 'db/store/sorddt.db'], { stdio: 'inherit' });
+		spawn.sync('git', ['checkout', 'db/store/sstock.db'], { stdio: 'inherit' });
 		spawn.sync('git', ['pull'], { stdio: 'inherit' });
 		next();
 		spawn('pm2', ['restart', 'all'], { stdio: 'inherit' });

4a593d4331dc517a1c529fb9895c1803b1b72dfb
Yinyi Wu <yinyiwu0202@gmail.com>
2021-09-12 09:41:52 +0800

    fix: schedule/index.js

--- schedule/index.js
+++ schedule/index.js
@@ -6,10 +6,15 @@ const XLSX = require('xlsx');
 const Jimp = require('jimp');
 const axios = require('axios');
 
-schedule.scheduleJob('* * * * *', async function(){
+
+const jobFn = async function(){
   console.log('ocr job running!');
   const ary = fs.readdirSync(ROOT_DIR);
   const dir = ary[ary.length - 1];
+  if(!dir){
+    console.log('job complete!(no folder)');
+    return;
+  }
   const filePath = `${FINISH_DIR}/${dir}.xlsx`;
   const isFileExists = fs.existsSync(filePath);
   if(isFileExists){
@@ -37,9 +42,10 @@ schedule.scheduleJob('* * * * *', async function(){
       j =0;
       do {
         j++;
-        target = ws['D'+j];
+        target = ws['H'+j];
         if(target){
           target.l = { Target:target.v, Tooltip:"link to image" };
+          target.v = '原圖連結';
         }
       } while(target);
       await XLSX.writeFile({
@@ -50,6 +56,7 @@ schedule.scheduleJob('* * * * *', async function(){
       }, filePath);
     }
   } else {
+
     const { files, base64String } = await quickstart(`${ROOT_DIR}/${dir}`, FINISH_DIR);
     console.log('insert', files);
     const result = await visonAPI(base64String);
@@ -62,6 +69,7 @@ schedule.scheduleJob('* * * * *', async function(){
       target = ws['H'+j];
       if(target){
         target.l = { Target:target.v, Tooltip:"link to image" };
+        target.v = '原圖連結';
       }
     } while(target);
     await XLSX.writeFile({
@@ -71,48 +79,25 @@ schedule.scheduleJob('* * * * *', async function(){
       }
     }, filePath);
   }
-});
-
-
-
-
-
-
-
-
+  console.log('job complete!');
+};
+schedule.scheduleJob('* * * * *', jobFn);
 
 
 async function quickstart(sourcePath, destPath, filterFn = (file, idx) => {
   return true;
 }) {
-  // // Imports the Google Cloud client library
-  // const vision = require('@google-cloud/vision');
-
-  // // Creates a client
-  // const client = new vision.ImageAnnotatorClient({
-  //   keyFilename:'./yangyi/pure-environs-322906-4a15627c2614.json'
-  // });
-
-  // // Performs label detection on the image file
-  // const [result] = await client.labelDetection('./yangyi/2021820_210820_142.jpg');
-  // const labels = result.labelAnnotations;
-  // console.log('Labels:');
-  // labels.forEach(label => console.log(label.description));
-
-
-
   let files = fs.readdirSync(sourcePath);
-
-
 
   async function cutX(path) {
     const jimp = await Jimp.read(`${sourcePath}/${path}`);
+
     let cutL = 0;
-    for (let i = 0; i < jimp.getWidth(); i++) {
+    for (let i = 5; i < 100; i++) {
       const v = await jimp.getPixelColor(i, jimp.getHeight() / 1.5);
       const { r, g, b } = Jimp.intToRGBA(v);
-      // console.log(path, i, Jimp.intToRGBA(v));
-      if (r < 105 && g < 105 && b < 105) {
+      console.log(path, i, Jimp.intToRGBA(v));
+      if (r <= 115 && g <= 126 && b <= 123) {
         // console.log(path, i, Jimp.intToRGBA(v));
         cutL = i;
         break;
@@ -135,59 +120,39 @@ async function quickstart(sourcePath, destPath, filterFn = (file, idx) => {
     return { jimp, cutL, cutR };
   }
 
-  const fileFilter = files.filter(filterFn).filter((file, idx)=>idx<21);
+  const fileFilter = files.filter((file)=>file.endsWith('.jpg')).filter(filterFn).filter((file, idx)=>idx<21);
   const images = await Promise.all(fileFilter.map(async (path) => {
-    // const bitmap = fs.readFileSync(`./yangyi/${path}`);
-    // const jimp = await Jimp.read(`./yangyi/${path}`);
-    // let cut = 0;
-    // for(let i=0;i<jimp.getWidth()-500;i++){
-    //   const v = await jimp.getPixelColor(i, jimp.getHeight()/2);
-    //   const { r,g,b} = Jimp.intToRGBA(v);
-    //   if(r < 170 && g<170 && b< 170){
-    //     console.log(i, Jimp.intToRGBA(v));
-    //     cut = i;
-    //     break;
-    //   }
-    // }
-    // await jimp.crop(cut, 0, jimp.getWidth()-cut, jimp.getHeight());
-    // await jimp.write(`./yangyi_tmp/${path.replace('.jpg', 'X.jpg')}`);
-
-
-    // const jimp = await Jimp.read(`./yangyi/${path}`);
 
     //去邊距
     const { jimp, cutL: cX, cutR } = await cutX(path);
     await jimp.crop(cX, 0, cutR, jimp.getHeight() / 1.9);
 
     await jimp.crop(0, jimp.getHeight() / 2, jimp.getWidth(), jimp.getHeight() / 2);
-    await jimp.crop(0, jimp.getHeight() / 3, jimp.getWidth() / 1.4, jimp.getHeight() / 3 * 2 - 1);
+
+    await jimp.crop(0, Math.floor(jimp.getHeight() / 4 * 3) + 1, jimp.getWidth() / 1.4, Math.floor(jimp.getHeight() / 4) -1 );
+    await jimp.write(`${destPath}/${path.replace('.jpg', 'A.jpg')}`);
+
 
     const jimp2 = await Jimp.read(`${sourcePath}/${path}`);
     await jimp2.crop(cX, jimp2.getHeight() / 1.9, (jimp2.getWidth()), jimp2.getHeight() / 4.5);
-
+    await jimp2.write(`${destPath}/${path.replace('.jpg', 'B.jpg')}`);
     const W = jimp.getWidth();
     const H = jimp.getHeight() + jimp2.getHeight();
 
     const spaceJimp = new Jimp(80, H, '#FFFFFF');
-
 
     const jimpCombo = new Jimp(W, H, '#FFFFFF');
     await jimpCombo.blit(jimp, 0, 0);
     await jimpCombo.blit(jimp2, 0, jimp.getHeight());
     await jimpCombo.blit(spaceJimp, 0, 0);
 
-
-    await jimpCombo.color([
-      { apply: 'xor', params: ['#F0F0F0'] },
-      // { 'apply':'lighten', params: [-20]},
-      // { 'apply':'tint', params: [40]
-    ]);
+    await jimpCombo.sepia();
+    await jimpCombo.invert();
     await jimpCombo.scale(0.6);
 
-    // await jimpCombo.write(`${destPath}/${path.replace('.jpg', 'F.jpg')}`);
+    await jimpCombo.write(`${destPath}/${path.replace('.jpg', 'F.jpg')}`);
     return jimpCombo;
   }));
-
 
   const { height, width } = images.reduce((sum, jimp) => {
     sum.height += jimp.getHeight();
@@ -195,50 +160,17 @@ async function quickstart(sourcePath, destPath, filterFn = (file, idx) => {
     return sum;
   }, { height: 0, width: 0 });
 
-
-  // console.log(width, height);
   const docJimp = new Jimp(width, height, '#000000');
   let h = 0;
   for (const jimp of images) {
-    // console.log(h);
     await docJimp.blit(jimp, 0, h);
     h += jimp.getHeight();
-    // await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK).then((f)=>docJimp.print(f, 0, h, '--Split--'));
-    // h+=40;
   }
-  // await docJimp.greyscale();
-  // docJimp.color([
-  //   // { 'apply': 'brighten', params:[10] },
-  //   // { 'apply': 'darken', params: [20] },
-  //   // { 'apply': 'greyscale', params: [] }
-  // ]);
 
   // await docJimp.write(`${FINISH_DIR}/F.jpg`);
   const v1 = await docJimp.getBase64Async(Jimp.MIME_JPEG);
 
   return { files:fileFilter.map(file=>`${sourcePath}/${file}`), base64String: v1.replace('data:image/jpeg;base64,', '')};
-
-
-
-
-  // const requests = files.filter((file,idx)=>{
-  //   return idx > 151;
-  // }).map((path)=>{
-  //   const bitmap = fs.readFileSync(`./yangyi/${path}`);
-  //   return {
-  //     features: [{ type: "TEXT_DETECTION" }],
-  //     imageContext: {
-  //       languageHints: ['zh-Hant']
-  //     },
-  //     image: {
-  //       content: Buffer.from(bitmap).toString('base64')
-  //     }
-  //   }
-  // });
-
-  // console.log(requests);
-  // return ;
-
 
 }
 
